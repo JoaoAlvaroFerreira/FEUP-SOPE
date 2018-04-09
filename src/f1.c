7dfc9acdead5f193335d632ef3120118536129e5
neto22 <pneto.98@gmail.com>
2018-04-09 17:53:21 +0100

    Exit function not perfect

--- src/f1.c
+++ src/f1.c
@@ -13,10 +13,42 @@
 
 //SOPE
 
-void sigint_handler(int sig){}
+void sigint_handler(int sig) { //main handler
+
+	char c;
+
+	printf("\nAre you sure you want to terminate (Y/N)? ");
+
+	scanf("%c", &c);
+
+	if (c == 'y' || c == 'Y')
+		exit(0);
+	else if (c == 'n' || c == 'N') {
+        fflush(stdin);
+		return;
+    }
+	else {
+		printf(" >> ERROR: Character not allowed!");
+		sigint_handler(SIGINT);
+	}
+	return;
+}
 
 void fileAction(char* dirName, char* name){}
 
 void process_info(char* dirName, struct dirent *direntp, struct stat stat_buf) {}
 
-int main(int argc, char* argv[]){}
+int main(){
+
+    	//Handling signal
+	struct sigaction action;
+	action.sa_handler = sigint_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+	signal(SIGINT, sigint_handler);
+
+    while(1) {
+        sleep(1);
+    }
+    exit(0);
+}
