b85566dea2fd661c41bd3c1500407400aecdf1a6
neto22 <pneto.98@gmail.com>
2018-04-12 23:17:54 +0100

    line contains word function

--- src/f1.c
+++ src/f1.c
@@ -34,21 +34,52 @@ void sigint_handler(int sig) { //main handler
 	return;
 }
 
-void fileAction(char* dirName, char* name){}
+int contains(char *line, char *word) {
 
-void process_info(char* dirName, struct dirent *direntp, struct stat stat_buf) {}
+	int i = 0;
+	int j = 0;
+
+	while(line[i] != '\n') {
+		if (line[i] == word[0]) {
+			j = 1;
+			i++;
+			while(word[j] != NULL) {
+				if (line[i] != word[j]) {
+					break;
+				}
+				i++;
+				j++;
+			}
+
+			if (word[j] == NULL) {
+				return 1;
+			}
+		} else
+			i++;
+	}
+
+	return 0;
+}
 
 int main(){
 
-    	//Handling signal
-	struct sigaction action;
-	action.sa_handler = sigint_handler;
-	sigemptyset(&action.sa_mask);
-	action.sa_flags = 0;
-	signal(SIGINT, sigint_handler);
+    // //Handling signal
+	// struct sigaction action;
+	// action.sa_handler = sigint_handler;
+	// sigemptyset(&action.sa_mask);
+	// action.sa_flags = 0;
+	// signal(SIGINT, sigint_handler);
 
-    while(1) {
-        sleep(1);
-    }
+    // while(1) {
+    //     sleep(1);
+    // }
+
+	char line[] = "Somebody once told me the world is gonna roll me\n";
+	char word[] = "body";
+
+	 if (contains(&line, &word) == 1)
+		printf("%s", line);
+	else
+		printf("Not found\n");
     exit(0);
 }
